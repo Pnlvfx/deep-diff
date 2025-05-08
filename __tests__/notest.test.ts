822d4536806e705e7f69659fbf60ca6155bd7b2f
simone <simonegauli@gmail.com>
2025-05-08 17:49:40 +0200

    add better types

---
+++ __tests__/notest.test.ts
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+import { describe, it } from '@jest/globals';
+
+describe('no-test', () => {
+  it('no-test', () => {
+    console.log('no-test');
+  });
+});
---
+++ tools/unused.ts
@@ -0,0 +1,10 @@
+import { findUnusedExports } from '@goatjs/ts-unused-exports';
+
+const unused = findUnusedExports({
+  ignoreFiles: ['eslint.config.js', 'jest.config.ts', 'deep-diff.ts'],
+  ignoreVars: [],
+});
+
+if (unused) {
+  throw new Error(`The following exports are unused, add them on the ignore or remove the exports to continue.\n${JSON.stringify(unused)}`);
+}
