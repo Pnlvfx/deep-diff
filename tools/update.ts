1710573c11d063477869d47d648cebf9e3b6242c
simone <simonegauli@gmail.com>
2025-07-09 03:59:59 +0200

    bump deps

---
+++ tools/update.ts
@@ -0,0 +1,9 @@
+import { updateLocalDeps } from '@goatjs/updater';
+
+await updateLocalDeps({
+  '@goatjs/eslint': 'github:Pnlvfx/eslint',
+  '@goatjs/rimraf': 'github:Pnlvfx/goatjs#workspace=@goatjs/rimraf',
+  '@goatjs/ts-unused-exports': 'github:Pnlvfx/goatjs#workspace=@goatjs/ts-unused-exports',
+  '@goatjs/typescript-config': 'github:Pnlvfx/typescript-config',
+  '@goatjs/updater': 'github:Pnlvfx/goatjs#workspace=@goatjs/updater',
+});
--- update.md
+++
@@ -1,3 +0,0 @@
-yarn up @goatjs/ts-unused-exports@github:Pnlvfx/goatjs#workspace=@goatjs/ts-unused-exports @goatjs/typescript-config@github:Pnlvfx/typescript-config @goatjs/eslint@github:Pnlvfx/eslint
-
-yarn upgrade-interactive
